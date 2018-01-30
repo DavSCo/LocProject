21de0cafb208b706b8a52772482a78543b985499
sara ayache <ayachesara@hotmail.com>
2018-01-30 11:55:24 +0100

    categorie

--- Platforme/Controllers/connectionController.php
+++ Platforme/Controllers/connectionController.php
@@ -7,7 +7,9 @@ $connect = new UserSQL();
 $vend = new VendeurSQL();
 
 
-if ($_POST["Categorie"] == "Entreprise" && (!empty($_POST))) {
+if (isset($_POST["Categorie"])&& ($_POST["Categorie"]== "Entreprise") && (!empty($_POST))) {
+    
+    $Categorie = $_POST['Categorie'];
     $compteVendeur = $vend->connectionVendeur();
 
     if (count($compteVendeur) > 0) {
@@ -16,21 +18,22 @@ if ($_POST["Categorie"] == "Entreprise" && (!empty($_POST))) {
         $_SESSION['particulier'] = false;
         $_SESSION['connected'] = true;
         $_SESSION['id'] = $compteVendeur[0]['id'];
-
         header('Location: index.php');
+    
 
     }
 
-} elseif ($_POST["Categorie"] == "Particulier" && (!empty($_POST))) {
-
+} elseif (isset($_POST["Categorie"]) && ($_POST["Categorie"]== "Particulier") && (!empty($_POST))) {
     $compte = $connect->connectionCompte();
+
+    if (count($compte) > 0) {
     $_SESSION['entreprise'] = false;
     $_SESSION['particulier'] = true;
     $_SESSION['connected'] = true;
     $_SESSION['id'] = $compte[0]['id'];
-
-
     header('Location: index.php');
+
+}
 
 } else {
     $_SESSION['connected'] = false;
